6dbad12723f1846c2006f68fed7c6d10190fd9ee
egan2015 <yangang@4g-live.com>
2016-06-14 00:01:54 +0800

    main changed

--- main.c
+++ main.c
@@ -7,12 +7,14 @@
 int main( int argc , char ** argv)
 {
 	fprintf(stderr,"This is a test for MPEG TS Stream...\n");
-	block_t *p_buffer = block_Alloc( 1024 );
 	
-	strcpy((char*)p_buffer->p_buffer,"hello block_t");
-	
-	fprintf(stderr,"Test block_t : %s,%zu\n",(char*)p_buffer->p_buffer,p_buffer->i_buffer);
-	
+  char *p = "hello block_t";
+  unsigned char *pp;
+	block_t *p_buffer = block_Alloc( strlen(p) );
+  pp=p_buffer->p_buffer;
+	p_buffer->p_buffer = (unsigned char *)p;
+  fprintf(stderr,"Test block_t : %s,%zu\n",(char*)p_buffer->p_buffer,p_buffer->i_buffer);
+	p_buffer->p_buffer = pp;
 	block_Release(p_buffer);
 	
 	return 0;
